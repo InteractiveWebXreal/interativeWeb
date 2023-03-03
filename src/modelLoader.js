051b6144bcfeff8ed8319bae37225b72214838ad
jessi68 <lyn990312@gmail.com>
2023-03-03 16:35:30 +0900

    chracter, bloock apply

--- src/modelLoader.js
+++ src/modelLoader.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-import { PointLight } from 'three';
+import { DirectionalLight, PointLight } from 'three';
+import { LAYER } from './consts/enum';
+import { on } from 'events';
 
 const loaders = {
     "obj": new OBJLoader(),
@@ -14,7 +16,7 @@ function getExtension(path) {
 }
 
 // 모델로딩이 api 불러오는 것처럼 비동기로 일어나서 async await 을 사용했습니다. 
-export async function loadModel(scene, path, scale) {
+export async function loadModel( path, scale, isCharacter) {
     let extension = getExtension(path);
    
     let loader = loaders[extension];
@@ -25,23 +27,29 @@ export async function loadModel(scene, path, scale) {
         path,
         // called when resource is loaded
         function ( object ) {
+            object = object.clone(true);
             object.scale.setScalar(scale);
             object.position.x -= 2;
     
+            
             object.traverse(function (child) {
-                console.log(child)
-                if(child instanceof PointLight) {
-                    if(child.name === "Light" || child.name === "영역") {
-                 
-                    object.remove(child)
-                    }
-                    if(child.name === "태양") {
-                       child.intensity = 0
-                        //child.intensity = 10
-                    }
+                // 캐릭터 빛 있는 상태로 전달..부탁..
+                
+                if( child instanceof PointLight) {
+                    //object.remove(child)
+                    child.intensity = 0;
                 }
+              
+                else if(child instanceof DirectionalLight) {
+                    child.intensity = 0.9
                
+                } else {
+
+                    
+                }
             });
+
+          
             resolve(object);
         },
         // called when loading is in progresses
@@ -52,6 +60,7 @@ export async function loadModel(scene, path, scale) {
         },
         // called when loading has errors
         function ( error ) {
+            console.log(error);
             console.log( 'An error happened' );
     
         }
---
+++ src/realPlayer.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three'
+import { LAYER } from './consts/enum';
+export class RealPlayer {
+    constructor(object) {
+        this.group = new THREE.Object3D();
+
+        let characterLight = new THREE.DirectionalLight(0xffffff, 0.1);
+        characterLight.castShadow = true;
+        console.log(characterLight.position)
+    
+        this.player = object;
+
+        this.player.scale.multiplyScalar(0.03);
+         this.player.position.x = -0.5;
+         this.player.position.y = -1;
+         this.player.position.z = 0.5;
+       // this.player.rotateOnAxis(axis, -1);
+        this.group.add(this.player);
+        this.group.add(characterLight)
+         // 크기 너무 커서 작게 조절
+        this.isHavingBox = false;
+        this.box = null;
+    }
+    getPosition() {
+        return this.group.position;
+    }
+
+    move(value) {
+        this.group.position.x += value.x; 
+        this.group.position.y += value.y; 
+        this.group.position.z += value.z; 
+        if(this.box != null) {
+            this.box.position.x += value.x; 
+            this.box.position.y += value.y; 
+            this.box.position.z += value.z; 
+        }
+    }
+    getObject() {
+        return this.group
+    }
+    tryHoldObject(box) {
+        // 박스 이미 들고 있으면 다른 박스 못 옮기게 하기
+        if(this.isHavingBox) {
+            return false;
+        }
+        box.position.x = this.group.position.x + 0.5;
+        box.position.y += 2.1;
+        this.box = box;
+        this.isHavingBox = true;
+        return true;
+    }
+    tryPutDown(plane) {
+        if(!this.isHavingBox)  {
+            return;
+        }
+        let playerPosition = this.group.position.clone();
+        playerPosition.y = plane.position.y
+
+        if(plane.position.distanceTo(playerPosition) < 0.1 && plane.material.color.equals(this.box.material.color)) {
+
+            this.box.position.x = plane.position.x;
+            this.box.position.y = plane.position.y + 0.4;
+            this.box.position.z = plane.position.z;
+            this.player.box = null
+            this.isHavingBox = false;
+        }
+    }
+}
